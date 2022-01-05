64f9a9a9b2cc2e01399aacbc639b5be4285973a7
Abhishek <abhishekbhartirocks1@gmail.com>
2022-01-05 15:23:13 +0530

    Minor bug fixes

--- app/src/main/java/com/androrubin/troika/nitjclubhub/CreatePost.kt
+++ app/src/main/java/com/androrubin/troika/nitjclubhub/CreatePost.kt
@@ -3,6 +3,7 @@ package com.androrubin.troika.nitjclubhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -25,30 +26,41 @@ class CreatePost : AppCompatActivity() {
         val title= findViewById<EditText>(R.id.title3)
         val des = findViewById<EditText>(R.id.descrition_mes)
 
+
         var name = intent.getStringExtra("collection_name")
 
         val submit = findViewById<Button>(R.id.submit)
         submit?.setOnClickListener {
 
 //            Toast.makeText(this,title.getText().toString(),Toast.LENGTH_SHORT).show()
-            val data = hashMapOf(
-                "title" to title.getText().toString(),
-                "description" to des.getText().toString(),
-                "date" to FieldValue.serverTimestamp()
-            )
+            if(TextUtils.isEmpty(title.getText().trim().toString())) {
 
-            db.collection("$name")
-                .add(data)
-                .addOnSuccessListener { documentReference ->
-                    Log.d("Data Addition", "DocumentSnapshot written with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    Log.w("Data Addition", "Error adding document", e)
-                }
-            val intent = Intent(this,MainActivity::class.java)
-            Toast.makeText(this,"Post Added",Toast.LENGTH_SHORT).show()
-            startActivity(intent)
-           finishAffinity()
+                title.setError("Title cannot be empty")
+                title.requestFocus()
+            }else if (TextUtils.isEmpty(des.getText().trim().toString())){
+                des.setError("Description cannot be empty")
+                des.requestFocus()
+            }
+            else{
+                    val data = hashMapOf(
+                        "title" to title.getText().toString(),
+                        "description" to des.getText().toString(),
+                        "date" to FieldValue.serverTimestamp()
+                    )
+                    db.collection("$name")
+                        .add(data)
+                        .addOnSuccessListener { documentReference ->
+                            Log.d("Data Addition", "DocumentSnapshot written with ID: ${documentReference.id}")
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w("Data Addition", "Error adding document", e)
+                        }
+                    val intent = Intent(this,MainActivity::class.java)
+                    Toast.makeText(this,"Post Added",Toast.LENGTH_SHORT).show()
+                    startActivity(intent)
+                    finishAffinity()
+            }
+
         }
     }
 }
